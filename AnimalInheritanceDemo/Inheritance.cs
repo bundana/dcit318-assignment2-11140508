47b9eb8a2a63806b85b1815bdd267e5851b635a7
bundana <bundanaabdulhafiz@gmail.com>
2025-07-31 10:27:52 +0000

    Task 1: Inheritance and Method Overriding - Animal, Dog, and Cat classes with method overriding

---
+++ AnimalInheritanceDemo/Inheritance.cs
@@ -0,0 +1,29 @@
+using System;
+
+namespace AnimalInheritanceDemo
+{
+    // Task 1: Inheritance and Method Overriding
+    public class Animal
+    {
+        public virtual void MakeSound()
+        {
+            Console.WriteLine("Some generic sound");
+        }
+    }
+
+    public class Dog : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Bark");
+        }
+    }
+
+    public class Cat : Animal
+    {
+        public override void MakeSound()
+        {
+            Console.WriteLine("Meow");
+        }
+    }
+}
