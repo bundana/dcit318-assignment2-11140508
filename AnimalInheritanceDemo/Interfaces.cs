0eae3714be2dfd13685dd1e4b0e0493360dc83f6
bundana <bundanaabdulhafiz@gmail.com>
2025-07-31 10:28:02 +0000

    Task 3: Interfaces - IMovable interface with Car and Bicycle implementations

---
+++ AnimalInheritanceDemo/Interfaces.cs
@@ -0,0 +1,26 @@
+using System;
+
+namespace AnimalInheritanceDemo
+{
+    // Task 3: Interfaces
+    public interface IMovable
+    {
+        void Move();
+    }
+
+    public class Car : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Car is moving");
+        }
+    }
+
+    public class Bicycle : IMovable
+    {
+        public void Move()
+        {
+            Console.WriteLine("Bicycle is moving");
+        }
+    }
+} 
